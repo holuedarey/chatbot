d1c46035a6c3bbf0b00d530eceafec2697e66f04
ogunwole samuel <samuel.ogunwole@iisysgroup.com>
2019-10-25 11:48:40 +0100

    changes for package

--- dialogflow.js
+++ dialogflow.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const axios = require('axios')
 
 const accessToken = process.env.DIALOGFLOW_ACCESS_TOKEN
@@ -16,23 +15,4 @@ module.exports = {
       headers: { Authorization: `Bearer ${accessToken}` }
     })
   }
-=======
-const axios = require('axios')
-
-const accessToken = process.env.DIALOGFLOW_ACCESS_TOKEN
-const baseURL = 'https://api.dialogflow.com/v1/query?v=20150910'
-
-module.exports = {
-  send (message) {
-    const data = {
-      query: message,
-      lang: 'en',
-      sessionId: '123456789!@#$%'
-    }
-
-    return axios.post(baseURL, data, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
-  }
->>>>>>> 8f237653135d5e4e3e2562819d1f34b124004f5d
 }
